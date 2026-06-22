97bfc483d1eca3fcab0824757c05f9296dcea6d1
Deepak <darkdeepak001@gmail.com>
2026-06-22 21:51:37 +0530

    Enhance account management features in script.js

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ let accounts =
 JSON.parse(
     sessionStorage.getItem("accounts")
 ) || []
-let currentAccount = JSON.parse(sessionStorage.getItem("currentAccount"))
-document.getElementById("welcome-user").textContent = "Welcome " + currentAccount.name
+let currentAccount =
+JSON.parse(sessionStorage.getItem("currentAccount"))
+
+let welcomeUser =
+document.getElementById("welcome-user")
+
+if(currentAccount && welcomeUser)
+{
+    welcomeUser.textContent =
+    "Welcome " + currentAccount.name
+}
 var nameinput = document.getElementById("name-input")
 var ageinput = document.getElementById("age-input")
 var phoneinput = document.getElementById("phone-input")
@@ -12,6 +21,62 @@ var depostinput = document.getElementById("deposit-input")
 var accountNumberDisplay = document.getElementById("account-number-display")
 var popup = document.getElementById("create-account-popup") 
 var accountnumberinput = document.getElementById("account-number-input")
+var balanceDisplay = document.getElementById("balance-display")
+if(balanceDisplay && currentAccount)
+{
+    balanceDisplay.textContent =
+    "Current Balance : ₹" + currentAccount.balance
+}
+var depositAmountInput =
+document.getElementById("depsosit-amount-input")
+
+var depositBtn =
+document.getElementById("deposit-btn")
+if(depositBtn)
+{
+    depositBtn.onclick = function()
+    {
+        currentAccount.balance +=
+        Number(depositAmountInput.value)
+
+        sessionStorage.setItem(
+            "currentAccount",
+            JSON.stringify(currentAccount)
+        )
+
+        alert("Deposit Successful")
+        depositAmountInput.value=""
+    }
+}
+var withdrawAmountInput =
+document.getElementById("withdraw-amount-input")
+
+var withdrawBtn =
+document.getElementById("withdraw-btn")
+if(withdrawBtn)
+{
+    withdrawBtn.onclick = function()
+    {
+        let amount =
+        Number(withdrawAmountInput.value)
+
+        if(amount > currentAccount.balance)
+        {
+            alert("Insufficient Balance")
+            return
+        }
+
+        currentAccount.balance -= amount
+
+        sessionStorage.setItem(
+            "currentAccount",
+            JSON.stringify(currentAccount)
+        )
+
+        alert("Withdrawal Successful")
+        withdrawAmountInput.value=""
+    }
+}
 function createAccount(event)
 {
     event.preventDefault()
